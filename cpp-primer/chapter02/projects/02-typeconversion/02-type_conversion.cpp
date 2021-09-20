a121aea15d1ca5e87357065ae50e7471c29f0f5e
acbgzm <acbgzm@126.com>
2021-09-20 22:40:27 +0800

    update

--- cpp-primer/chapter02/projects/02-typeconversion/02-type_conversion.cpp
+++ cpp-primer/chapter02/projects/02-typeconversion/02-type_conversion.cpp
@@ -1,13 +1,25 @@
 #include<iostream>
 
 int main() {
+	/*
 	bool b = 42;	// b=ture
 	int i = b;		// i=1
 	i = 3.83;		// i=3
 	double pi = i;	// pi=3.0
 	
-	unsigned char c1 = -1;	// 
-	signed char c2 = 256;
+	unsigned char c1 = -1;	// 255
+	signed char c2 = 256;	// undefined
+	*/
+
+	unsigned u = 10, u2 = 42;
+	std::cout << u2 - u << std::endl;
+	std::cout << u - u2 << std::endl;
+
+	int i = 10, i2 = 42;
+	std::cout << i2 - i << std::endl;
+	std::cout << i - i2 << std::endl;
+	std::cout << i - u << std::endl;
+	std::cout << u - i << std::endl;
 
 	return 0;
 }
