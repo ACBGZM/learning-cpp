0ba10890e2bc044c3e5493a803e4d29b78e123e5
acbgzm <acbgzm@126.com>
2021-09-20 20:38:37 +0800

    update

---
+++ cpp-primer/chapter02/projects/02-typeconversion/02-type_conversion.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+	bool b = 42;	// b=ture
+	int i = b;		// i=1
+	i = 3.83;		// i=3
+	double pi = i;	// pi=3.0
+	
+	unsigned char c1 = -1;	// 
+	signed char c2 = 256;
+
+	return 0;
+}
