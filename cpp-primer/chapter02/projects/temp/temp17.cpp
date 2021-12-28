1c0df17a2d7d2372b85de08a8db824fd44ea0bb8
ACBGZM <ACBGZM@126.com>
2021-12-28 21:42:31 +0800

    update

--- cpp-primer/chapter02/projects/temp/temp17.cpp
+++ cpp-primer/chapter02/projects/temp/temp17.cpp
@@ -22,10 +22,32 @@ struct S3
 	double b;
 	int a;
 };
+
+class MyClass
+{
+	int i;
+	char j;
+	double b;
+	int a;
+	virtual void foo()=0;
+};
+
 int main()
 {
 	printf("%d\n", sizeof(S1)); // 输出24
 	printf("%d\n", sizeof(S1)); // 输出24
 	printf("%d\n", sizeof(S3)); // 输出24
+
+	printf("%d\n", sizeof(MyClass)); // 输出24
+
+	char* p1 = (char*)"hello";
+	char p2[] = "hello";
+	
+	// p1[0] = 'e'; // 错误，p1位于栈区、指向常量区，不能修改常量区的值
+
+	p2[0] = 'e';	// 正确，p2位于栈区，拷贝了常量区的值到栈区
+
+	printf("%s\n", p2);
+
 	return 0;
 }
